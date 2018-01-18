51ae816756f06c4e5b6018dafb328de436e4ce32
mddemarie <mddemarie@gmail.com>
2018-01-18 18:11:20 +0100

    connect the database & load the model & import and register the route

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    User = require('./users-api/models/model'),
+    bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/db');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const routes = require('./users-api/routes/route');
+routes(app);
 
 app.listen(port);
 
