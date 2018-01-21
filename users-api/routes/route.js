baaf465c49a7ce02e2cc685ff6a70ec0d8329192
mddemarie <mddemarie@gmail.com>
2018-01-21 18:40:18 +0100

    change the API route to /api/users

--- users-api/routes/route.js
+++ users-api/routes/route.js
@@ -3,11 +3,11 @@ module.exports = function(app) {
     const userRoutes = require('../controllers/controller');
 
     // User Routes
-    app.route('/users')
+    app.route('/api/users')
         .get(userRoutes.list_all_users)
         .post(userRoutes.create_a_user);
 
-    app.route('/users/:taskId')
+    app.route('/api/users/:userId')
         .get(userRoutes.read_a_user)
         .put(userRoutes.update_a_user)
         .delete(userRoutes.delete_a_user)
