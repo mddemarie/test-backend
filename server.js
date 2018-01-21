a3761eddc4729c3902d5fa027afb2d5ae52afc90
mddemarie <mddemarie@gmail.com>
2018-01-21 18:39:38 +0100

    fix the bug with 404 & import express router & restructured the code

--- server.js
+++ server.js
@@ -3,24 +3,26 @@ const express = require('express'),
     port = process.env.PORT || 3000,
     mongoose = require('mongoose'),
     User = require('./users-api/models/model'),
+    routes = require('./users-api/routes/route'),
     bodyParser = require('body-parser'),
+    router = express.Router();
     swaggerUi = require('swagger-ui-express'),
     swaggerDocument = require('./swagger.json');
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/db');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(function(req, res) {
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api', router);
+
+routes(app);
+
+app.use(function(req, res, next) {
     res.status(404).send({url: req.originalUrl + ' not found'})
 });
 
-const routes = require('./users-api/routes/route');
-routes(app);
-
 app.listen(port);
 
-console.log('user data RESTful API server started on:' + port);
+console.log('user data RESTful API server started on: ' + port);
